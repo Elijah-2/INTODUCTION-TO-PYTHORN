ce0b4fd16e966feff0b1b17b985e7f077b61e1fd
Elijah-2 <elijahoumaonyango220@gmail.com>
2025-01-17 15:49:10 +0300

    short notes --lesson 1.1
    introduction to python short notes

---
+++ hello.py
@@ -0,0 +1,26 @@
+name=input("whats your name ? ")
+#print("hello, " + name )
+#print("hello ", name )# multiple arguments space is inserted automatically
+#print("hello , ",end="")# makes the end of the string blank
+#print("hello , ",name,sep=" ")#separate string
+# remove white space from a string, capitalize
+#name=name.strip()
+#name=name.capitalize()
+#name=name.title()
+# we can combine all this in one
+name=name.strip().title()# this is the method chaining
+print(f"hello, {name}") 
+
+
+
+
+
+# arguments are inputs to a function that might influence its behaviour
+#outputs are the side effects of the python programming
+# bugs -these are errors in the code
+# reurn values-values intended to return by the function
+# variable is a container for values in a computer
+#comments - notes in the code or the program
+#pseudocode-outline how the progrum is going to be programmed
+# strings -str->sequence of characters
+#\ this is the escape characters
